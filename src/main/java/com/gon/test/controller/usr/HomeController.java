ae5d175179f8f12c8ae40d513cd72c6f90bd6b24
free-gooodz <freegon92@gmail.com>
2022-04-21 11:27:27 +0900

    생성

---
+++ src/main/java/com/gon/test/controller/usr/HomeController.java
@@ -0,0 +1,19 @@
+package com.gon.test.controller.usr;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+	@RequestMapping("/usr/home/main")
+	@ResponseBody
+	public String showMain() {
+		return "안녕";
+	}
+	@RequestMapping("/usr/home/plus")
+	@ResponseBody
+	public int showPlus(int a, int b) {
+		return a + b;
+	}
+}
